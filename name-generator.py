e1211ab7957450f829201f7a0c8337af7fdad29a
Jake Greene <jakeandrew.greene@gmail.com>
2016-04-29 17:29:15 -0400

    Name Creation: Create Gendered Names

--- name-generator.py
+++ name-generator.py
@@ -4,22 +4,55 @@ import collections
 import argparse
 
 
-Name = collections.namedtuple('Name', ['text', 'meaning'])
-Affix = collections.namedtuple('Affix', ['text', 'meaning'])
+Name = collections.namedtuple('Name', ['text', 'gender', 'meaning'])
+Affix = collections.namedtuple('Affix', ['text', 'gender', 'meaning'])
+
+
+class NameCreationException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+def select_optional_property(affix_dict, prop):
+    p = None
+    if prop in affix_dict:
+        p = affix_dict[prop].encode('utf-8')
+    return p
 
 
 # Choose an affix from the sequence of affixations
 #
 # Expects the affix sequence to be in the format:
-# [ { 'options': ['affix1', 'affix2', ...], 'meaning': 'text describing affix' }, ... ]
+# [
+#   {
+#     'options': ['affix spelling', 'affix alternative spelling', ...],
+#     'meaning': 'optional text describing affix',
+#     'gender': 'optional gender'
+#   },
+#   ...
+# ]
 #
 # Returns an Affix
 #
 def choose_affix(affixations):
     affix_dict = random.choice(affixations)
     affix = random.choice(affix_dict['options']).encode('utf-8')
-    meaning = affix_dict['meaning'].encode('utf-8')
-    return Affix(affix, meaning)
+    meaning = select_optional_property(affix_dict, 'meaning')
+    gender = select_optional_property(affix_dict, 'gender')
+    return Affix(affix, gender, meaning)
+
+
+# Select all affixations that either match the given gender or are unisex
+def select_proper_affixations(name_data, affix_type, gender=None):
+    affixations = name_data['proper_names'][affix_type]
+    if gender is not None:
+        affixations = [a for a in affixations if 'gender' not in a or a['gender'] == gender]
+    if len(affixations) == 0:
+        raise NameCreationException("No " + affix_type + " for gender "+ gender)
+    return affixations
 
 
 # Randomly generate a name from the provided data
@@ -28,21 +61,32 @@ def choose_affix(affixations):
 # { 'race': 'name of race',
 #   'sources': ['source of the data', 'other source', ...],
 #   'proper_names': {
-#     'prefixes': [ { 'options': ['affix1', 'affix2', ...], 'meaning': 'text describing affix' }, ... ]
-#     'suffixes': [ { 'options': ['affix1', 'affix2', ...], 'meaning': 'text describing affix' }, ... ]
+#     'prefixes': [ { 'options': ['affix1', ...], 'meaning': 'text describing affix', 'gender': 'optional gender' }, ... ]
+#     'suffixes': [ { 'options': ['affix1', ...], 'meaning': 'text describing affix', 'gender': 'optional gender' }, ... ]
 #   }
 #   'surnames': {
-#     'prefixes': [ { 'options': ['affix1', 'affix2', ...], 'meaning': 'text describing affix' }, ... ]
-#     'suffixes': [ { 'options': ['affix1', 'affix2', ...], 'meaning': 'text describing affix' }, ... ]
+#     'prefixes': [ { 'options': ['affix1', ...], 'meaning': 'text describing affix', 'gender': 'optional gender' }, ... ]
+#     'suffixes': [ { 'options': ['affix1', ...], 'meaning': 'text describing affix', 'gender': 'optional gender' }, ... ]
 #   }
 # }
 #
-def create_name(name_data):
-    prefix = choose_affix(name_data['proper_names']['prefixes'])
-    suffix = choose_affix(name_data['proper_names']['suffixes'])
+def create_name(name_data, gender=None):
+    prefixes = select_proper_affixations(name_data, "prefixes", gender)
+    prefix = choose_affix(prefixes)
+    # The suffix gender must be equal the most refined gender between `gender` and `prefix.gender`
+    # in order to avoid mixed-gender names
+    suffix_selection_gender = gender
+    if prefix.gender is not None:
+        suffix_selection_gender = prefix.gender
+    suffixes = select_proper_affixations(name_data, "suffixes", suffix_selection_gender)
+    suffix = choose_affix(suffixes)
     name = prefix.text + suffix.text
     meaning = prefix.meaning + " " + suffix.meaning
-    return Name(name, meaning)
+    if prefix.gender is not None:
+        created_gender = prefix.gender
+    else:
+        created_gender = suffix.gender
+    return Name(name, created_gender, meaning)
 
 
 def pretty_print(rows):
@@ -54,9 +98,14 @@ def pretty_print(rows):
 parser = argparse.ArgumentParser(description="Name Generator")
 parser.add_argument("name_file", type=file, help="a file listing the possible components of a name")
 parser.add_argument("-n", dest="num_names", type=int, help="the number of names to create", default=10)
+gender_help = "limit the name creation to unisex names and gendered names equal to GENDER"
+parser.add_argument("-g", "--gender", dest="gender", help=gender_help, default=None)
 args = parser.parse_args()
 
-name_data = json.load(args.name_file)
-names = [create_name(name_data) for num in range(0, args.num_names)]
-header = [["Name", "Meaning"]]
-pretty_print(header + names)
+try:
+    name_data = json.load(args.name_file)
+    names = [create_name(name_data, args.gender) for num in range(0, args.num_names)]
+    header = [["Name", "Gender", "Meaning"]]
+    pretty_print(header + names)
+except NameCreationException as e:
+    print "Could not create names due to error. " + e.value
