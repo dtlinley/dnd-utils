d09b0331e8b10a865a0455029dd8b60512caf9fb
David Linley <dtlinley@gmail.com>
2016-05-07 23:40:24 -0400

    Expand on halfling traits

--- character_randomizer/halfling_traits.py
+++ character_randomizer/halfling_traits.py
@@ -1,5 +1,19 @@
 import common_traits
 
+hair_colours = [
+    {'attribute': 'brown hair', 'weight': 1, 'subtraits': []},
+    {'attribute': 'sandy-brown hair', 'weight': 1, 'subtraits': []},
+    {'attribute': 'dirty-blonde hair', 'weight': 1, 'subtraits': []},
+    {'attribute': 'dark brown hair', 'weight': 1, 'subtraits': []},
+]
+
+skin_colours = [
+    {'attribute': 'tan', 'weight': 1, 'subtraits': [hair_colours]},
+    {'attribute': 'light tan', 'weight': 1, 'subtraits': [hair_colours]},
+    {'attribute': 'ruddy skin', 'weight': 1, 'subtraits': [hair_colours]},
+    {'attribute': 'reddish-pale skin', 'weight': 1, 'subtraits': [hair_colours]},
+]
+
 subraces = [
     {'attribute': 'stout halfling', 'weight': 1, 'subtraits': []},
     {'attribute': 'lightfoot halfling', 'weight': 1, 'subtraits': []},
@@ -8,5 +22,5 @@ subraces = [
 traits = [
     subraces,
     common_traits.physical_characteristics,
-    common_traits.skin_colours
+    skin_colours,
 ]
